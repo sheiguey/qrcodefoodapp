23c8e159c5c2b6f1fc8a28d124824724315bdcf0
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-03-18 19:15:01 +0100

    test

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -2,7 +2,7 @@ import { BASE_URL } from "../config/site-settings";
 
 export const fetcher = async (url, options) => {
   const params = new URLSearchParams(options);
-  return await fetch(`${BASE_URL}${url}${options ? `?${params.toString()}` : ""}`, {
+  return await fetch(`${url}${options ? `?${params.toString()}` : ""}`, {
     headers: {
       Application: "application/json",
     },
